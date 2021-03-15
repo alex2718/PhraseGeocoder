7488c34e1cb221440fc49e3a9fbf813ebd07ca4c
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-15 20:54:49 +1100

    Update example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import results
 from phrasegeo import Matcher
 
 # load up the db
-db_name = 'GNAF_today'
+db_name = 'GNAF_VIC'
 DB = f"postgresql:///{db_name}"
 db = results.db(DB)
 
@@ -16,6 +16,8 @@ addresslist = list(df['ADDRESS'].values)
 
 # set up the geocoder if this hasn't already been done
 # i.e., create the phrase and inverted index tables
+# will throw an error if this has already been setup
+# or if the GNAF has not been setup correctly
 matcher.setup()
 
 # do the matching
