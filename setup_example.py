d60e9dc376c5b2fd464a453d03c7e25f06ad0f08
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-15 20:55:04 +1100

    Update setup script

--- setup_example.py
+++ setup_example.py
@@ -1,14 +1,15 @@
 # set up the GNAF database
 from phrasegeo import GNAFLoader
 
-gnaf = GNAFLoader("GNAF_VIC")
+# specify the name of the database
+gnaf = GNAFLoader("postgresql:///GNAF_VIC")
 
 # path to the auth and the state psv files
-gnaf_path = ''
-gnaf_aut_path = ''
+gnaf_path = '/Users/vic2e3a/Desktop/Data/GNAF/nov20_gnaf_pipeseparatedvalue/G-NAF/G-NAF NOVEMBER 2020/Standard'
+gnaf_aut_path = '/Users/vic2e3a/Desktop/Data/GNAF/nov20_gnaf_pipeseparatedvalue/G-NAF/G-NAF NOVEMBER 2020/Authority Code'
 
 # load up the psv files
-gnaf.load_data(self, gnaf_path, gnaf_aut_path, state_names=['VIC'])
+gnaf.load_data(gnaf_path, gnaf_aut_path, state_names=['VIC'])
 
 # add the FK contraints
 gnaf.add_constraints()
