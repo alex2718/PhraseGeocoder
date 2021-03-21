27759e5c679820a6e17bb5f14ada8b1c96d5354e
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-21 18:48:52 +1100

    Remove Vic postcodes from input tokens and make small improvements to speed

--- queries/do_match4.sql
+++ queries/do_match4.sql
@@ -8,7 +8,8 @@ WITH input_addresses_with_numerics AS (
     (
         SELECT address_id, array_agg(i) as numeric_tokens
         FROM (
-            SELECT t1.address_id, (regexp_matches(t1.address, '\d+', 'g'))[1] i 
+            SELECT t1.address_id, (regexp_matches(
+                regexp_replace(t1.address, '3\d{3}$', ''), '\d+', 'g'))[1] i 
             FROM input_addresses t1
         ) temp
         GROUP BY address_id
@@ -63,8 +64,8 @@ final_match AS (
     ORDER BY t1.address_id1 ASC, t1.similarity DESC
 ),
 best_match AS (
-    SELECT DISTINCT t1.address_id1, t1.address as input_address, t1.matched_address, 
-    t1.similarity as similarity,
+    SELECT DISTINCT on (t1.address_id1) address_id1, t1.address as input_address, 
+    t1.matched_address, t1.similarity as similarity,
     street_locality_pid, locality_pid, building_name, 
     lot_number_prefix, lot_number, lot_number_suffix, 
     flat_type, flat_number_prefix, flat_number, flat_number_suffix, 
@@ -76,11 +77,6 @@ best_match AS (
     locality_name, state_abbreviation, postcode, latitude, longitude, 
     geocode_type, alias_principal, primary_secondary, legal_parcel_id, date_created
     FROM final_match t1
-    INNER JOIN (
-        SELECT address_id1, max(similarity) as similarity
-        FROM final_match
-        GROUP BY address_id1
-    ) t2
-    ON t1.address_id1=t2.address_id1 AND t2.similarity=t1.similarity
+    ORDER BY address_id1 ASC, similarity DESC
 )
 SELECT * FROM best_match;
