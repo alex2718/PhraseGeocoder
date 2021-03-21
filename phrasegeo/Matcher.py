d22b01ed2384158e1f20e7884a306207b3a2c6f5
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-21 15:09:10 +1100

    Change query being used

--- phrasegeo/Matcher.py
+++ phrasegeo/Matcher.py
@@ -1,7 +1,7 @@
 import results 
 from pathlib import Path
 
-DO_MATCH = Path("queries/do_match.sql").read_text()
+DO_MATCH = Path("queries/do_match4.sql").read_text()
 CREATE_GEOCODER_TABLES = Path("queries/create_geocoder_tables.sql").read_text()
 MAKE_ADDRESSES = Path("queries/make_addresses.sql").read_text()
 CREATE_PHRASES = Path("queries/create_phrases.sql").read_text()
