6a88be043c51a0fafb4e89f4768a6a13cc8895c1
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-22 20:13:19 +1100

    Update setup method so that trigram tables are created

--- phrasegeo/Matcher.py
+++ phrasegeo/Matcher.py
@@ -3,11 +3,17 @@ from pathlib import Path
 
 DO_MATCH_BASIC = Path("queries/do_match4.sql").read_text()
 DO_MATCH_TRIGRAM = Path("queries/do_trigram_match2.sql").read_text() # trigram phrases
+
 CREATE_GEOCODER_TABLES = Path("queries/create_geocoder_tables.sql").read_text()
 MAKE_ADDRESSES = Path("queries/make_addresses.sql").read_text()
+
 CREATE_PHRASES = Path("queries/create_phrases.sql").read_text()
 INVERTED_INDEX = Path("queries/phrase_inverted.sql").read_text()
 CREATE_INDEXES = Path("queries/create_indexes.sql").read_text()
+
+CREATE_TRIGRAMPHRASES = Path("queries/create_trigram_phrases.sql").read_text()
+TRIGRAMINVERTED_INDEX = Path("queries/trigram_phrase_inverted.sql").read_text()
+CREATE_TRIGRAMINDEXES = Path("queries/create_trigram_indexes.sql").read_text()
 
 # todo: create a BaseMatcher class and inherit all matches from this
 class Matcher(object):
@@ -28,13 +34,19 @@ class Matcher(object):
         
         print('Creating phrases...')
         self.db.ss(CREATE_PHRASES)
+
+        print('Creating trigram phrases...')
+        self.db.ss(CREATE_TRIGRAMPHRASES)
         
         print('Creating inverted index...')
         # create inverted index
         self.db.ss(INVERTED_INDEX)
-        
+        # create inverted index for trigram phrases
+        self.db.ss(TRIGRAMINVERTED_INDEX)
+
         # create indexes
         self.db.ss(CREATE_INDEXES)
+        self.db.ss(CREATE_TRIGRAMINDEXES)
 
     def match(self, address_list, how='standard'):
         with self.db.transaction() as t:
