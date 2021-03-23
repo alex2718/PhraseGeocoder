6f1b8fc5af9a235f1043fb283e23904bec6feb3e
alex.lee <alex.lee@dpc.vic.gov.au>
2021-03-23 23:52:57 +1100

    Allow for address_id values to be specified as part of match method

--- phrasegeo/Matcher.py
+++ phrasegeo/Matcher.py
@@ -1,8 +1,9 @@
 import results 
 from pathlib import Path
 
-DO_MATCH_BASIC = Path("queries/do_match4.sql").read_text()
-DO_MATCH_TRIGRAM = Path("queries/do_trigram_match2.sql").read_text() # trigram phrases
+DO_MATCH_BASIC = Path("queries/do_match4.sql").read_text() # threshold 400 - for fast matching
+DO_MATCH_SLOW = Path("queries/do_match_slower.sql").read_text() # threshold 10000 - for slower but more accurate
+DO_MATCH_TRIGRAM = Path("queries/do_trigram_match2.sql").read_text() # trigram phrases - slowest but most accurate
 
 CREATE_GEOCODER_TABLES = Path("queries/create_geocoder_tables.sql").read_text()
 MAKE_ADDRESSES = Path("queries/make_addresses.sql").read_text()
@@ -17,12 +18,13 @@ CREATE_TRIGRAMINDEXES = Path("queries/create_trigram_indexes.sql").read_text()
 
 # todo: create a BaseMatcher class and inherit all matches from this
 class Matcher(object):
-    def __init__(self, db=None, initialize=False, threshold=0.5):
+    def __init__(self, db=None, how='standard', initialize=False, threshold=0.5):
         """
         Initialize the matcher object. Requires a DB to connect to
         """
         self.db = db 
         self.threshold = threshold # the threshold when chaining matchers together
+        self.how = how # which SQL query to use
 
     def setup(self):
         """
@@ -48,21 +50,35 @@ class Matcher(object):
         self.db.ss(CREATE_INDEXES)
         self.db.ss(CREATE_TRIGRAMINDEXES)
 
-    def match(self, address_list, how='standard'):
+    def match(self, addresses, address_ids=None):
+        how = self.how
+        
         with self.db.transaction() as t:
             t.ex(
                 "create temporary table input_addresses(address_id bigint not null, address text not null);"
             )
 
-            input_list = [
-                dict(address_id=i, address=a.upper())
-                for i, a in enumerate(address_list)
-            ]
+            if address_ids:
+                input_list = [
+                    dict(address_id=i, address=a.upper())
+                    for i, a in zip(address_ids, addresses)  
+                    ]
+            else:
+                input_list = [
+                    dict(address_id=i, address=a.upper())
+                    for i, a in enumerate(addresses)
+                    ]
+
             t.insert("input_addresses", input_list)
             if how == 'standard':
                 answers = t.ex(DO_MATCH_BASIC)
             elif how == 'trigram':
                 answers = t.ex(DO_MATCH_TRIGRAM)
+            elif how == 'slow':
+                answers = t.ex(DO_MATCH_SLOW)
+            else:
+                print(f'No query for {how} matching, using standard phrase geocoder')
+                answers = t.ex(DO_MATCH_BASIC)
             t.ex("drop table input_addresses;")
         return answers
 
