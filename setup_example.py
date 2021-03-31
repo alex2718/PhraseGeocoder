a1e314902807660ba4fa266796457df18a262a4a
alex2718 <ajlee3141@gmail.com>
2021-04-01 09:03:42 +1100

    Small changes

--- setup_example.py
+++ setup_example.py
@@ -2,14 +2,15 @@
 from phrasegeo import GNAFLoader
 
 # specify the name of the database
-gnaf = GNAFLoader("postgresql:///GNAF_VIC")
+gnaf = GNAFLoader("postgresql:///GNAF_VICNSW")
 
 # path to the auth and the state psv files
-gnaf_path = '/GNAF/nov20_gnaf_pipeseparatedvalue/G-NAF/G-NAF NOVEMBER 2020/Standard'
-gnaf_aut_path = '/GNAF/nov20_gnaf_pipeseparatedvalue/G-NAF/G-NAF NOVEMBER 2020/Authority Code'
+base_path = "/Users/vic2e3a/Desktop/Data"
+gnaf_path = f"{base_path}/GNAF/FEB21_GNAF_PipeSeparatedValue_20210222101749/G-NAF/G-NAF FEBRUARY 2021/Standard"
+gnaf_aut_path = f"{base_path}/GNAF/FEB21_GNAF_PipeSeparatedValue_20210222101749/G-NAF/G-NAF FEBRUARY 2021/Authority Code"
 
 # load up the psv files
-gnaf.load_data(gnaf_path, gnaf_aut_path, state_names=['VIC'])
+gnaf.load_data(gnaf_path, gnaf_aut_path, state_names=["VIC", "NSW"])
 
 # add the FK contraints
 gnaf.add_constraints()
